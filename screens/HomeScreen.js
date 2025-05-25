1c686f4895d8591c801e5b3a7a9c176c3ffd58fc
therealmanraj <singhmanraj8@gmail.com>
2025-05-25 02:23:46 -0700

    update with apk

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -35,7 +35,26 @@ const DEMO_PATIENTS = [
     riskPct: "90%",
     riskColor: "#e33e3e",
   },
-  // …and so on
+  {
+    id: "3",
+    name: "John Smith",
+    details: "Age: 72, Male",
+    avatar:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuAMxlcPx1dh-l8uhb9AvXQovks5QvuaDw8nQlNHdEw_ih_jmt_2yAteEVaYa_QoEYS2XdZP9qbacppKkhvpbjflNr5hFfJUesihfdQQYkAYqCiNI5UvX_0bVNf1xIMQhz2xoLLJH7iD98IGVJhWnK4KGLaZ7zqs2wEWexXOJNMXOiy4fb6iidqojcOZhk_0jKQHefiuQZ474WDRqZA_xlK48aEtbPm4-lIkL9ObBX_ip1h4feAHQb58WPaw4NLs-B7A4cwKjVCcvnE",
+    riskLabel: "Low",
+    riskPct: "10%",
+    riskColor: "#0AD95C",
+  },
+  {
+    id: "4",
+    name: "Olivia Brown",
+    details: "Age: 20, Female",
+    avatar:
+      "https://lh3.googleusercontent.com/aida-public/AB6AXuB9OboGcvAk2beHqEABPJ8Z-O-qesZemOu11NYQidOkhTrQ-pU_fCa1ZMirdpiQKTD9FceLQOQn0E2i0sS2SuqTColDfZz_oCczDvgLgleKYn9vijRA64GaNSqw9fv0Eg-2zeg-OMsi1oj6_jZ94Zms60Fon893UgswtZCEkqv7oobFaQRkTcty47mBJcPvzm-WCHCyvUxuT5MYFviOdfG2nqOZe-CjU9KxCgDtnIbnFnvgvoInzIWCxZEglukRqJu-KsDwwe4WIiA",
+    riskLabel: "Low",
+    riskPct: "25%",
+    riskColor: "#0AD95C",
+  },
 ];
 
 export default function HomeScreen({ navigation }) {
